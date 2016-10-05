5339ca8abbc66cb6117e6d3a736adf350560664f
Daibogh <DaiBogh@gmail.com>
2016-10-05 09:02:30 +0300

    add vector ptr in functions

--- Kozhevnikov/simplenumbers/main.cpp
+++ Kozhevnikov/simplenumbers/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
-void simplenums(int num)
+void simplenums(int num, vector<int> &ptr)
 {
-	int k;
+	int k = 0;
+	cout<<"test\n";
 	for (int i = 2 ; i <= num ; ++i)
 	{
 		for (int j = 2 ; j < i ; ++j )
@@ -15,12 +16,18 @@ void simplenums(int num)
 		if (k)
 			k = 0;
 		else
-			cout<<i<<endl;
+		{
+			// cout<<"test\n";
+			ptr.push_back(i);
+			// cout<<"test\n";
+			// cout<<i<<endl;
+		}
 	}
 }
-void one_more_simple(int num)
+void one_more_simple(int num,vector<int> &ptr)
 {
-	simplenums(num);
+
+	simplenums(num,ptr);
 	int k = 0;
 	while (1)
 	{
@@ -36,7 +43,8 @@ void one_more_simple(int num)
 		}
 		else
 		{
-			cout<<num<<endl;	
+			ptr.push_back(num);
+			// cout<<num<<endl;	
 			break;
 		}
 	}
@@ -44,8 +52,13 @@ void one_more_simple(int num)
 
 int main(int argc, char const *argv[])
 {
+	vector<int> ptr;
 	int num;
 	cin >> num;
-	one_more_simple(num);
+	simplenums(num,ptr);
+	// one_more_simple(num,ptr.begin());
+
+	for (int i = 0 ; i < ptr.size(); i++)
+		cout<<ptr[i]<<endl;;
 	return 0;
 }
