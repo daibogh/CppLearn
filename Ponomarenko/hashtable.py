959ac4eb026c3bf2ee2866ddfad2499074b2721e
daibogh <=>
2016-10-09 22:52:30 +0300

    sht

--- Ponomarenko/hashtable.py
+++ Ponomarenko/hashtable.py
@@ -1,8 +1,11 @@
 import numpy
+
+import ctypes
+m = ctypes.c_wchar_p * (ctypes.c_uint(2).value+1)
 class hashtable1(object):
 	def __init__(self):
-		self.List = numpy.zeros(10**20)
-		print(self.List)
+		self.List = m(1)
+		print(self.List.contents)
 		pass
 	def __HashFunction(self,key):
 		return self.__Ly(key)
@@ -18,11 +21,11 @@ class hashtable1(object):
 		return self.List[IntKey]
 	# different hash functions
 	def __Ly(self,key):
-		Hash = 0
+		Hash = ctypes.c_uint(0)
 		for i in key:
-			Hash = (Hash * 1664525) + ord(i) + 1013904223
+			Hash = ctypes.c_uint(Hash.value * 1664525 + ord(i) + 1013904223)
 		print(Hash)
-		return Hash
+		return Hash.value
 h = hashtable1()
-h.AddObject("petr",1997)
-print(h.Get("petr"))
+# h.AddObject("petr",1997)
+# print(h.Get("petr"))
